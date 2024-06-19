67f3741d832828493b3e426b931cd59910a7bc6f
James DeFoggia <jdefoggia@gmail.com>
2024-06-19 17:19:27 -0400

    views.py linting

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,10 +3,10 @@
 # from django.shortcuts import render
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.contrib import messages
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -31,7 +31,11 @@ def login_user(request):
     username = data['userName']
     password = data['password']
     # Try to check if provide credential can be authenticated
-    user = authenticate(username=username, password=password)
+    try:
+        user = authenticate(username=username, password=password)
+    except Exception as e:
+        logger.error("Error authenticating user: {}".format(e))
+        return JsonResponse({"status": 500, "message": "Internal Server Error"})
     data = {"userName": username}
     if user is not None:
         # If user is valid, call login method to login current user
@@ -42,7 +46,11 @@ def login_user(request):
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-    logout(request)
+    try:
+        logout(request)
+    except Exception as e:
+        logger.error("Error logging out user: {}".format(e))
+        return JsonResponse({"status": 500, "message": "Internal Server Error"})
     data = {"userName": ""}
     return JsonResponse(data)
 
@@ -58,76 +66,92 @@ def registration(request):
     first_name = data['firstName']
     last_name = data['lastName']
     email = data['email']
-    # username_exist = False
-    # email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
-        # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
+    except User.DoesNotExist:
+        username_exist = False
 
     # If it is a new user
     if not username_exist:
-        # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
-        # Login the user and redirect to list page
-        login(request, user)
-        data = {"userName": username,"status": "Authenticated"}
-        return JsonResponse(data)
+        try:
+            # Create user in auth_user table
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+            # Login the user and redirect to list page
+            login(request, user)
+            data = {"userName": username,"status": "Authenticated"}
+            return JsonResponse(data)
+        except Exception as e:
+            logger.error("Error creating user: {}".format(e))
+            return JsonResponse({"status": 500, "message": "Internal Server Error"})
     else :
         data = {"userName": username,"error": "Already Registered"}
         return JsonResponse(data)
 
 def get_cars(request):
-    count = CarMake.objects.filter().count()
-    print(count)
-    if(count == 0): 
-        initiate()
-    car_models = CarModel.objects.select_related('car_make')
-    cars = []
-    for car_model in car_models: 
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels": cars})
+    try:
+        count = CarMake.objects.filter().count()
+        if(count == 0): 
+            initiate()
+        car_models = CarModel.objects.select_related('car_make')
+        cars = []
+        for car_model in car_models: 
+            cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        return JsonResponse({"CarModels": cars})
+    except Exception as e:
+        logger.error("Error getting cars: {}".format(e))
+        return JsonResponse({"status": 500, "message": "Internal Server Error"})
 
 #Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state = "All"):
-    if(state == "All"):
-        endpoint = "/fetchDealers"
-    else:
-        endpoint = "/fetchDealers/"+state
-    dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200, "dealers": dealerships})
+    try:
+        if(state == "All"):
+            endpoint = "/fetchDealers"
+        else:
+            endpoint = "/fetchDealers/"+state
+        dealerships = get_request(endpoint)
+        return JsonResponse({"status": 200, "dealers": dealerships})
+    except Exception as e:
+        logger.error("Error getting dealerships: {}".format(e))
+        return JsonResponse({"status": 500, "message": "Internal Server Error"})
 
 def get_dealer_details(request, dealer_id):
-    if(dealer_id): 
-        endpoint = "/fetchDealer/"+str(dealer_id)
-        dealership = get_request(endpoint)
-        return JsonResponse({"status": 200, "dealer": dealership})
-    else: 
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+    try:
+        if(dealer_id): 
+            endpoint = "/fetchDealer/"+str(dealer_id)
+            dealership = get_request(endpoint)
+            return JsonResponse({"status": 200, "dealer": dealership})
+        else: 
+            return JsonResponse({"status": 400, "message": "Bad Request"})
+    except Exception as e:
+        logger.error("Error getting dealer details: {}".format(e))
+        return JsonResponse({"status": 500, "message": "Internal Server Error"})
 
 def get_dealer_reviews(request, dealer_id):
-    # if dealer id has been provided
-    if(dealer_id): 
-        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
-        reviews = get_request(endpoint)
-        for review_detail in reviews: 
-            response = analyze_review_sentiments(review_detail['review'])
-            print(response)
-            review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status": 200, "reviews": reviews})
-    else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
+    try:
+        # if dealer id has been provided
+        if(dealer_id): 
+            endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+            reviews = get_request(endpoint)
+            for review_detail in reviews: 
+                response = analyze_review_sentiments(review_detail['review'])
+                review_detail['sentiment'] = response['sentiment']
+            return JsonResponse({"status": 200, "reviews": reviews})
+        else:
+            return JsonResponse({"status": 400, "message": "Bad Request"})
+    except Exception as e:
+        logger.error("Error getting dealer reviews: {}".format(e))
+        return JsonResponse({"status": 500, "message": "Internal Server Error"})
 
 def add_review(request): 
-    if(request.user.is_anonymous == False): 
-        data = json.loads(request.body)
-        try:
+    try:
+        if(request.user.is_anonymous == False): 
+            data = json.loads(request.body)
             response = post_review(data)
             return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
-    else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
+        else:
+            return JsonResponse({"status": 403, "message": "Unauthorized"})
+    except Exception as e:
+        logger.error("Error adding review: {}".format(e))
+        return JsonResponse({"status": 500, "message": "Internal Server Error"})
