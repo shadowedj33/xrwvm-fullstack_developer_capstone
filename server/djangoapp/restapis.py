8fde152c8254ce8c0b1e674703cda3667d070a1c
James DeFoggia <jdefoggia@gmail.com>
2024-06-19 16:17:53 -0400

    more linting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,11 +12,11 @@ sentiment_analyzer_url = os.getenv(
     default="http://localhost:5050/")
 
 
-def get_request(endpoint, **kwargs): 
+def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params= params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
 
